ae2ee1db6a9089a9ebc6505f502ea08c4e23d63e
Keks <keks@htmlacademy.ru>
2022-11-22 17:59:59 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,11 @@
+import {similarOffer} from './data.js';
+import {createCardElement} from './card.js';
+import {deactivatePage, deactivateMapFilters, activatePage, activateMapFilters} from './form.js';
+import './form.js';
+
+similarOffer();
+createCardElement();
+deactivatePage();
+deactivateMapFilters();
+activatePage();
+activateMapFilters();
