d8012a1c0d317c15ac4b9330a96036e5361c885b
Anna Semenova <a.semenova2018@gmail.com>
2022-11-23 21:39:43 +0700

    Изменение дз

--- js/form.js
+++ js/form.js
@@ -1,9 +1,3 @@
-/* eslint-disable no-undef */
-// Заведите модуль, который будет отвечать за работу с формой.
-
-// Реализуйте с помощью JavaScript (удобнее функцией!) перевод страницы в неактивное состояние. Все пункты, кроме первого про карту.
-// Важно. Неактивность должна добавляться именно средствами JavaScript, иначе, если классы и атрибуты добавить напрямую в HTML, при ошибке в скриптах или ошибке загрузки скриптов сайт будет недоступен пользователю.
-
 import './form-validator';
 import './data.js';
 
@@ -12,17 +6,13 @@ const adForm = document.querySelector('.ad-form');
 const fieldset = adForm.querySelectorAll('fieldset');
 const adress = adForm.querySelector('#address');
 
-// 1.1. Неактивное состояние. При открытии страница находится в неактивном состоянии:
 const deactivatePage = () => {
-  // Форма заполнения информации об объявлении .ad-form содержит класс ad-form--disabled;
   adForm.classList.add('ad-form--disabled');
-  // Все интерактивные элементы формы .ad-form должны быть заблокированы с помощью атрибута disabled, добавленного на них или на их родительские блоки fieldset. Слайдер также должен быть заблокирован ???? какой слайдер))));
   fieldset.forEach((element) => {
     element.disabled = true;
   });
 };
 
-// Форма с фильтрами .map__filters заблокирована так же, как и форма .ad-form — на форму добавлен специальный класс, а на её интерактивные элементы атрибуты disabled.
 const MapFilters = document.querySelector('.map__filters');
 const select = MapFilters.querySelectorAll('select');
 
@@ -36,17 +26,8 @@ const deactivateMapFilters = () => {
 deactivatePage();
 deactivateMapFilters();
 
-// Напишите функцию, которая будет переводить страницу в активное состояние. В задании про карту мы будем вызывать эту функцию, когда карта будет загружена и готова к работе, а пока вы можете просто вызвать эту функцию активации в своём коде.
-
-// 1.2. Активное состояние. Загрузка и успешная инициализация карты (карта реализуется сторонней библиотекой Leaflet) переводит страницу в активное состояние. В активном состоянии страница позволяет:
-
-// Вносить изменения в форму и отправлять её на сервер;
-// После загрузки данных с сервера просматривать похожие объявления на карте, фильтровать их и уточнять подробную информацию о них, показывая для каждого из объявлений карточку.
-
 const activatePage = () => {
-  // Форма заполнения информации об объявлении .ad-form содержит класс ad-form--disabled;
   adForm.classList.remove('ad-form--disabled');
-  // Все интерактивные элементы формы .ad-form должны быть заблокированы с помощью атрибута disabled, добавленного на них или на их родительские блоки fieldset. Слайдер также должен быть заблокирован
   fieldset.forEach((element) => {
     element.disabled = false;
   });
@@ -62,10 +43,10 @@ const activateMapFilters = () => {
 activatePage();
 activateMapFilters();
 
-const numberPoint = 5;
+const NUMBER_POINT = 5;
 
 const setAdress = (coordinates) => {
-  adress.value = `${coordinates.lat.toFixed(numberPoint)}, ${coordinates.lng.toFixed(numberPoint)}`;
+  adress.value = `${coordinates.lat.toFixed(NUMBER_POINT)}, ${coordinates.lng.toFixed(NUMBER_POINT)}`;
 };
 
 export {deactivatePage, deactivateMapFilters, activatePage, activateMapFilters, setAdress};
